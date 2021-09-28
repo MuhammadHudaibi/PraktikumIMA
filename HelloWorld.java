8e003364c56c505322576ee577450719d596f2c0
MuhammmadHudibi <muhhudaibi11@gmail.con>
2021-09-29 06:49:49 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Muhammad Hudaibi") ;
+		System.out.println("6706213010") ;
+		System.out.println("D3RPLA04") ;
+	}
+}
